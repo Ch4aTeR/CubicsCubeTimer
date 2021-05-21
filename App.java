063ee59eba9701d076c7fa1ca4f2ed561b3d983c
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2021-05-21 14:14:53 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public Stage primaryStage;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        showLogin();
+        this.primaryStage.show();
+    }
+    public void showLogin() throws Exception {
+        Login login = new Login();
+        login.start(this);
+    }
+    public static void main(String[] args) throws Exception {
+        launch();
+    }
+    public void showTimer() {
+        Timer timer = new Timer();
+        try {
+            timer.start(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
